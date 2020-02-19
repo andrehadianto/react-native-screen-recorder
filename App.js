8f3de537c3b7c7044a6194ac43582d464826178d
Andre Hadianto <andre_hadianto@mymial.sutd.edu.sg>
2020-02-19 23:00:10 +0800

    add sample file saving button

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { 
+import React from "react";
+import {
   StyleSheet,
   Text,
   TextInput,
@@ -10,56 +10,58 @@ import {
   Platform,
   DeviceEventEmitter,
   Keyboard
-} from 'react-native';
-import VideoPlayer from 'react-native-video-controls';
+} from "react-native";
+import VideoPlayer from "react-native-video-controls";
+import * as RNFS from "react-native-fs";
+
 const { RecorderManager } = NativeModules;
 
-export default class App extends React.Component {
+class App extends React.Component {
   state = {
     videoUri: null,
     disableStart: false,
     disableStopped: true,
     disablePlayable: true,
     androidVideoUrl: null,
-    keyboardIsShown: false,
-  }
+    keyboardIsShown: false
+  };
 
   start = () => {
     RecorderManager.start();
     this.setState({
       disableStart: true,
       disableStopped: false,
-      disablePlayable: true,
+      disablePlayable: true
     });
-  }
+  };
 
   stop = () => {
     RecorderManager.stop();
     this.setState({
       disableStart: true,
       disableStopped: true,
-      disablePlayable: false,
+      disablePlayable: false
     });
-  }
+  };
 
   play = () => {
     switch (Platform.OS) {
-      case 'android':
-        const { androidVideoUrl } = this.state;   
+      case "android":
+        const { androidVideoUrl } = this.state;
         if (androidVideoUrl) {
           this.setState({
             videoUri: androidVideoUrl,
             disableStart: true,
             disableStopped: true,
-            disablePlayable: true,
-          })
+            disablePlayable: true
+          });
         }
         break;
 
-      case 'ios':
+      case "ios":
         CameraRoll.getPhotos({
           first: 1,
-          assetType: 'Videos'
+          assetType: "Videos"
         }).then(r => {
           if (r.edges.length > 0) {
             const video = r.edges[0].node.image;
@@ -67,88 +69,150 @@ export default class App extends React.Component {
               videoUri: video.uri,
               disableStart: true,
               disableStopped: true,
-              disablePlayable: true,
-            })
+              disablePlayable: true
+            });
           }
         });
-      break;  
-    
+        break;
+
       default:
         break;
     }
+  };
+
+  writeFile() {
+    const path = RNFS.DocumentDirectoryPath + "/" + Date.now().toString();
+
+    RNFS.writeFile(path, "New photo", "utf8")
+      .then(success => {
+        console.log("File is written!");
+      })
+      .catch(err => {
+        console.warn(err.message);
+      });
   }
 
-  playEnded = () => {      
+  readFile() {
+    const path = RNFS.DocumentDirectoryPath + "/test.txt";
+
+    // RNFS.readFile(path, "utf8")
+    //   .then((res) => {
+    //     console.log("Finish reading");
+    //     console.log(res);
+    //   })
+    //   .catch(err => {
+    //     console.warn(err.message);
+    //   });
+
+      RNFS.readDir(RNFS.DocumentDirectoryPath)
+      .then(files => {
+        console.log(files);
+      })
+      .catch((err) => {
+        console.warn(err.message, err.code);
+      });
+  }
+
+  playEnded = () => {
     this.setState({
       videoUri: null,
       disableStart: false,
       disableStopped: true,
       disablePlayable: true,
-      androidVideoUrl: null,
+      androidVideoUrl: null
     });
-  }
+  };
 
   keyboardDidShow = () => {
-    this.setState({keyboardIsShown: true});
-  }
-  
+    this.setState({ keyboardIsShown: true });
+  };
+
   keyboardDidHide = () => {
-    this.setState({keyboardIsShown: false});
-  }
+    this.setState({ keyboardIsShown: false });
+  };
 
   rendernControlBtnGroup = () => {
     const { disableStart, disableStopped, disablePlayable } = this.state;
     return (
       <View style={styles.footer}>
-        <Button style={styles.button} disabled={disableStart} title="Start" onPress={this.start} />
-        <Button style={styles.button} disabled={disableStopped} title="Stop" onPress={this.stop} />
-        <Button style={styles.button} disabled={disablePlayable} title="Play" onPress={this.play} />
+        <Button
+          style={styles.button}
+          disabled={disableStart}
+          title="Start"
+          onPress={this.start}
+        />
+        <Button
+          style={styles.button}
+          disabled={disableStopped}
+          title="Stop"
+          onPress={this.stop}
+        />
+        <Button
+          style={styles.button}
+          disabled={disablePlayable}
+          title="Play"
+          onPress={this.play}
+        />
+        <Button style={styles.button} title="Write" onPress={this.writeFile} />
+        <Button style={styles.button} title="Read" onPress={this.readFile} />
       </View>
-    )
-  }
+    );
+  };
 
   componentWillMount() {
-    DeviceEventEmitter.addListener('updateFilePath', (filePath) => {
+    DeviceEventEmitter.addListener("updateFilePath", filePath => {
       console.log(filePath);
-      
-      this.setState({androidVideoUrl: filePath});  
+
+      this.setState({ androidVideoUrl: filePath });
     });
   }
 
-  componentDidMount () {
-    this.keyboardDidShowListener = Keyboard.addListener('keyboardDidShow', this.keyboardDidShow);
-    this.keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', this.keyboardDidHide);
+  componentDidMount() {
+    this.keyboardDidShowListener = Keyboard.addListener(
+      "keyboardDidShow",
+      this.keyboardDidShow
+    );
+    this.keyboardDidHideListener = Keyboard.addListener(
+      "keyboardDidHide",
+      this.keyboardDidHide
+    );
+
+    RNFS.readDir(RNFS.DocumentDirectoryPath)
+      .then(files => {
+        console.log(files);
+      })
+      .catch(err => {
+        console.warn(err.message, err.code);
+      });
   }
 
-  componentWillUnmount () {
+  componentWillUnmount() {
     this.keyboardDidShowListener.remove();
     this.keyboardDidHideListener.remove();
   }
-  
+
   render() {
     const { videoUri, keyboardIsShown } = this.state;
     return (
       <View style={styles.container}>
-        {Platform.OS === 'ios' && keyboardIsShown &&
-          this.rendernControlBtnGroup()
-        }
+        {Platform.OS === "ios" &&
+          keyboardIsShown &&
+          this.rendernControlBtnGroup()}
         <View style={styles.content}>
-          {videoUri && 
-            <VideoPlayer
-              source={{ uri: videoUri }}
-              onEnd={this.playEnded}
-            />
-          }
-          {!videoUri && 
+          {videoUri && (
+            <VideoPlayer source={{ uri: videoUri }} onEnd={this.playEnded} />
+          )}
+          {!videoUri && (
             <TextInput
               style={styles.textInput}
               multiline
               underlineColorAndroid="white"
-              onChangeText={(text) => this.setState({text})}
+              onChangeText={text => this.setState({ text })}
               value={this.state.text}
             />
-          }
+          )}
         </View>
+        <Text>{RNFS.DocumentDirectoryPath}</Text>
         {this.rendernControlBtnGroup()}
       </View>
     );
@@ -159,30 +223,32 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 20,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
 
   content: {
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: "row",
     padding: 20,
-    justifyContent: 'center',
+    justifyContent: "center"
   },
 
   textInput: {
-    borderColor: 'gray',
+    borderColor: "gray",
     borderWidth: 1,
     flex: 1,
     fontSize: 24
   },
 
   footer: {
-    backgroundColor: Platform.OS==='ios' ? '#eee' : '#fff',
-    flexDirection: 'row',
-    alignSelf: 'stretch',
-    justifyContent: 'center',
-    paddingVertical: 20,
-  },
+    backgroundColor: Platform.OS === "ios" ? "#eee" : "#fff",
+    flexDirection: "row",
+    alignSelf: "stretch",
+    justifyContent: "center",
+    paddingVertical: 20
+  }
 });
+
+export default App;
